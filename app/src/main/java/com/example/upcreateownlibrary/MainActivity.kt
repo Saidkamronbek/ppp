2dc3bc6c9bd18e089944e58aa8f49ab88a26694c
kama_001 <krahmatullayev69@gmail.com>
2022-06-30 00:00:15 -1200

    ozgaruvchi kirg`izdim

--- app/src/main/java/com/example/upcreateownlibrary/MainActivity.kt
+++ app/src/main/java/com/example/upcreateownlibrary/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        kerak bolgan saytlar
+//        https://jitpack.io/
+//        https://github.com/Saidkamronbek/ppp/releases/tag/1.0.0
+//        https://medium.com/@anujguptawork/how-to-create-your-own-android-library-and-publish-it-750e0f7481bf
     }
 }
